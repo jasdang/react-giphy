d075b1a3b2bc0be5e388744a9cd604cc038982ea
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-25 20:35:54 +0800

    added SearchBar component

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
+import App from './components/app.jsx';
+
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name='Jasmine' />, root);
+  ReactDOM.render(<App />, root);
 }
