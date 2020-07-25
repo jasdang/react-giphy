65e96e6f190270e1ed8c7e16319d77515668374d
Jasmine Dang <ngandth.1992@gmail.com>
2020-07-26 00:22:46 +0800

    call giphy-api when typing in search-bar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import SearchBar from './search-bar';
 import Gif from './gif';
 import GifList from './gif_list';
+const keys = require('../../keys.json');
+const giphy = require('giphy-api')(keys['GIPHY-API']);
 
 class App extends Component {
   constructor(props) {
@@ -11,12 +13,19 @@ class App extends Component {
       selectedGifId: 'QWMTY0yBemVRV9A26q',
     };
   }
+
+  search = (keyword) => {
+    giphy.search({q: keyword, rating: 'g', limit: 10}, (err, res) => {
+      // Res contains gif data!
+      this.setState({gifs: res.data});
+    });
+  };
+
   render() {
-    const gifs = [{id: '3rgXBroGwOuAScNQfm'}, {id: 'dZRI96NcCiAvjWEKBR'}];
     return (
       <div>
         <div className='left-scene'>
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className='selected-gif'>
             <Gif id={this.state.selectedGifId} />
           </div>
--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+  handleChange = (event) => {
+    const keyword = event.currentTarget.value;
+    this.props.searchFunction(keyword);
+  };
   render() {
-    return <input type='text' className='form-control form-search' />;
+    return (
+      <input
+        type='text'
+        className='form-control form-search'
+        onChange={this.handleChange}
+      />
+    );
   }
 }
 
